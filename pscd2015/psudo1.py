bddb21a1016f29fe9fbf3c046b5cbcd411e69f37
akash <akigupta131@gmail.com>
2015-04-04 18:25:13 +0530

    pseudoCode competition problem 1

--- pscd2015/psudo1.py
+++ pscd2015/psudo1.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
-n = int(raw_input())
+t = int(raw_input())
 case=0
-while (case<n):
+while (case<t):
 	total = 0
-	S = raw_input()
-	S_chars = list(S)
-	S_chars_len = len(S_chars)
-	for i in xrange(0,S_chars_len):
-		total += ord(S_chars[i])-64
+	times = raw_input().split()
+	times_len = len(times)
+	for i in xrange(0,times_len):
+		hh, mm = map(str,times[i].split(':'))
+		if hh==mm or hh==mm[::-1] or (list(hh)[0]==list(hh)[1] and list(mm)[0]==list(mm)[1]):
+			total += 1
 	print total
 	case = case+1
